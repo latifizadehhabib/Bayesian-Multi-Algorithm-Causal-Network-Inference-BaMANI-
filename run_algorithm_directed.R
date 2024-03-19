e6e284d6c0f42e606edd582e1eda790e00ea6c7b
Habib Latifizadeh <68330501+latifizadehhabib@users.noreply.github.com>
2024-03-19 17:09:06 -0400

    Add files via upload

--- run_algorithm_directed.R
+++ run_algorithm_directed.R
@@ -4,6 +4,7 @@ run_algorithm_directed <- function(algorithm_directed, Blank_edge_list, Edge_cou
   
   
   
+  
   # --------------------------------------------
     # Debug print statements:
   #   
@@ -18,6 +19,85 @@ run_algorithm_directed <- function(algorithm_directed, Blank_edge_list, Edge_cou
   # cat("White List rows: ", nrow(white_List), "\n")
   # cat("Black List NULL: ", is.null(Black_List), "\n")
   # cat("Black List rows: ", nrow(Black_List), "\n")
+  # -------------------------------------------- For DAGMA algorithm
+  # -----------------
+  # averaging adjacency matrices: (if each network is represented by an adjacency matrix)
+  average_networks <- function(network_list) {
+    if (length(network_list) == 0) {
+      stop("Network list is empty")
+    }
+    
+    n <- nrow(network_list[[1]])
+    for (network in network_list) {
+      if (nrow(network) != n || ncol(network) != n) {
+        stop("All networks must have same size")
+      }
+    }
+    
+    avg_matrix <- Reduce('+', network_list) / length(network_list)
+    return(avg_matrix)
+  }
+  # -----------------
+  # Function for running a bootstrap process with a given algorithm
+  run_bootstrap_dagma <- function(nboot, Black_List, white_List, discretized_data) {
+    bootstrap_results <- list()
+    
+    n_iterations <- min(nboot, 1)
+    
+    for(i in 1:n_iterations) {
+      bootstrap_sample <- discretized_data[sample(nrow(discretized_data), replace = TRUE), ]
+      
+      library(reticulate)
+
+      # Convert R data frames to Python objects and assign them to Python variables
+      py$bootstrap_sample_py <- r_to_py(bootstrap_sample)
+      py$Black_List_py <- r_to_py(Black_List)
+      py$white_List_py <- r_to_py(white_List)
+      
+      # Execute Python code
+      py_run_string("
+import pandas as pd
+from dagma.linear import DagmaLinear
+
+# Convert the R data frames to pandas DataFrames
+data = pd.DataFrame(bootstrap_sample_py)
+blacklist = pd.DataFrame(Black_List_py)
+whitelist = pd.DataFrame(white_List_py)
+
+# Create a mapping from column names to indices
+names_idx = {col: idx for idx, col in enumerate(data.columns)}
+
+# Initialize lists for excluded and included edges
+edges_ex = []
+edges_in = []
+
+# Populate the list of excluded edges from the blacklist
+n_blacklist, _ = blacklist.shape
+for i in range(n_blacklist):
+  row = blacklist.values[i].tolist()
+  edges_ex.append((names_idx[row[0]], names_idx[row[1]]))
+
+# Populate the list of included edges from the whitelist
+n_whitelist, _ = whitelist.shape
+for i in range(n_whitelist):
+  row = whitelist.values[i].tolist()
+  edges_in.append((names_idx[row[0]], names_idx[row[1]]))
+
+# Fit the model with both excluded and included edges
+model = DagmaLinear(loss_type='l2')
+W_est = model.fit(data.values, lambda1=0.1, exclude_edges=edges_ex, include_edges=edges_in)
+")
+      
+      # Retrieve the adjusted matrix and store it
+      # Here we directly access the Python object without converting, based on your previous preference
+      adj_matrix <- py$W_est
+      bootstrap_results[[i]] <- adj_matrix
+    }
+    
+    # Compute and return the averaged network from the bootstrap results
+    return(average_networks(bootstrap_results))
+  }
+  # -----------------
   # --------------------------------------------
   
   
@@ -30,22 +110,37 @@ run_algorithm_directed <- function(algorithm_directed, Blank_edge_list, Edge_cou
   # *---------------------------------------new 2
     
     for (algorithm in algorithm_directed) {
-      # white_list_present <- !is.na(white_List) && nrow(white_List) > 0
-      # white_list_present <- !is.null(white_List) && !is.na(white_List) && length(white_List) > 0
-      white_list_present <- !is.null(white_List) && nrow(white_List) > 0
-      
-      black_list_present <- !is.null(Black_List) && nrow(Black_List) > 0
-      if (white_list_present && black_list_present) {
-        arstr <- boot.strength(discretized_data, R = nboot, algorithm = algorithm, cluster = cl,
-                               algorithm.args = list(whitelist = white_List, blacklist = Black_List))
+      if (algorithm != "DAGMA") {
+        white_list_present <- !is.null(white_List) && nrow(white_List) > 0
+        black_list_present <- !is.null(Black_List) && nrow(Black_List) > 0
+        
+        if (white_list_present && black_list_present) {
+          arstr <- boot.strength(discretized_data, R = nboot, algorithm = algorithm, cluster = cl,
+                                 algorithm.args = list(whitelist = white_List, blacklist = Black_List))
         } else if (white_list_present) {
           arstr <- boot.strength(discretized_data, R = nboot, algorithm = algorithm, cluster = cl,
                                  algorithm.args = list(whitelist = white_List))
-          } else if(black_list_present) {
-            arstr <- boot.strength(discretized_data, R = nboot, algorithm = algorithm, cluster = cl,
-                                   algorithm.args = list(blacklist = Black_List))
-          }
-      
+        } else if(black_list_present) {
+          arstr <- boot.strength(discretized_data, R = nboot, algorithm = algorithm, cluster = cl,
+                                 algorithm.args = list(blacklist = Black_List))
+        }
+        
+      } else {
+        # Process DAGMA algorithm
+        # Note: DAGMA-specific functions and processing are called here
+        average_network_dagma <- run_bootstrap_dagma(nboot, Black_List, white_List, discretized_data)
+        average_network_dagma_edges <- as.data.frame(which(average_network_dagma != 0, arr.ind = TRUE))
+        names(average_network_dagma_edges) <- c("from", "to")
+        
+        # Map numeric indices to column names
+        col_names <- colnames(discretized_data)
+        average_network_dagma_edges$from <- col_names[average_network_dagma_edges$from]
+        average_network_dagma_edges$to <- col_names[average_network_dagma_edges$to]
+      }
+        # white_list_present <- !is.na(white_List) && nrow(white_List) > 0
+        # white_list_present <- !is.null(white_List) && !is.na(white_List) && length(white_List) > 0
+        
+        
       # }
   
   # ------------------------------------new 1
